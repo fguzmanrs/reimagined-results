6692655a6040b9cc87bcd25fb7d22bd6f0b2b3d1
Emily Yu <bluerainmango@gmail.com>
2020-02-15 12:42:19 -0800

    [backend] added encryption for password, jwt(only for signup)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,43 @@
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
 const catchAsync = require("../utill/catchAsync");
 
-exports.signup = catchAsync(async (req, res) => {});
-exports.login = catchAsync(async (req, res) => {});
-exports.logout = catchAsync(async (req, res) => {});
+exports.signup = catchAsync(async (req, res, next) => {
+  const { username, password } = req.body;
+
+  // Validation
+  if (!username || !password) {
+    return next(new Error("Please provide email and password."));
+  }
+
+  //* Encrypt user's password
+  const encryptedPwd = await bcrypt.hash(password, 12);
+
+  //! [Sequelize] Store new user info to DB (username, encryptedPwd)
+  //! [Sequelize] get new user's id for token creation: get it from the returned result of adding user info to DB(userId)
+
+  // will continue to work
+  const token = jwt.sign(
+    {
+      username
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JSWT_EXPIRES_IN }
+  );
+
+  console.log(token);
+
+  res.status(200).json({
+    status: "success",
+    message: "New user has been successfully created!",
+    token,
+    data: {
+      username
+    }
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {});
+
+exports.logout = catchAsync(async (req, res, next) => {});
 // exports.protect = catchAsync(async (req, res) => {});
--- utill/catchAsync.js
+++ utill/catchAsync.js
@@ -1,3 +1,4 @@
+// Catch error from asynchronous functions
 module.exports = function catchAsync(fn) {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
