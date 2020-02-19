8249c083e051a2ba510215792606575afcd33b38
Emily Yu <bluerainmango@gmail.com>
2020-02-18 20:38:37 -0800

    [backend] fixed catchAsync

--- utill/catchAsync.js
+++ utill/catchAsync.js
@@ -1,9 +1,5 @@
 // Catch error from asynchronous functions
-<<<<<<< HEAD
-module.exports = function catchAsync(req, res, next) {
-=======
 module.exports = fn => {
->>>>>>> backEnd
   return (req, res, next) => {
     fn(req, res, next).catch(next);
   };
