867e612caebbe51f2998407fd9996b9916db2e13
Fernando Ortiz <ffortizn@gmail.com>
2020-02-18 22:03:20 -0800

    Changes to database, unique constrains and fixed popularity:numeric(6,3)

--- models/1_discovered.js
+++ models/1_discovered.js
@@ -3,11 +3,10 @@ module.exports = function (sequelize, Sequelize) {
         id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
         userId: { type: Sequelize.INTEGER, references: { model: 'User', key: 'id', unique: 'u' } },
         movieId: { type: Sequelize.INTEGER, references: { model: 'Movie', key: 'id', unique: 'u' } }
-    }, {
-        uniqueKeys: { u: { fields: ['userId', 'movieId'] } }
-    }, {
-        freezeTableName: true
-    });
+    },
+        { freezeTableName: true },
+        { uniqueKeys: { u: { fields: ['userId', 'movieId'] } } });
+
     console.info("Sequelize: ./models/discovered.js");
     return Discovered;
 };
